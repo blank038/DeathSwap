3bc51de138bcaf03a24954d0755fb87b2177d8d2
Blank038 <admin@mc9y.com>
2020-10-26 23:44:46 +0800

    Initial commit

---
+++ src/main/java/com/blank038/deathswap/DeathSwap.java
@@ -0,0 +1,11 @@
+package com.blank038.deathswap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeathSwap extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
